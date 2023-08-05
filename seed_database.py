17e61bb7b5a878891a24f6c9fb7859f924d62acd
Bryan Ortega <fbryanortega@gmail.com>
2023-08-05 15:27:52 -0500

    Created and stored movies in db

--- seed_database.py
+++ seed_database.py
@@ -10,5 +10,34 @@ import model
 import server
 
 os.system("dropdb ratings")
+os.system('createdb ratings')
 
-# More code will go here
+model.connect_to_db(server.app)
+model.db.create_all()
+
+# Load movie data from JSON file
+with open('data/movies.json') as f:
+    movie_data = json.loads(f.read())
+
+# Create movies, store them in list so we can use them
+# to create fake ratings later
+movies_in_db = []
+for movie in movie_data:
+    # Get the title, overview, and poster_path from the movie dictionary. 
+    title = movie['title']
+    overview = movie['overview']
+    poster_path = movie['poster_path']
+
+    # Then, get the release_date and convert it to a
+    # datetime object with datetime.strptime
+    release_date = movie['release_date']
+    format = "%Y-%m-%d"
+
+    formatted_release_date = datetime.strptime(release_date, format)
+
+    # create a movie here and append it to movies_in_db
+    new_movie = crud.create_movie(title, overview, formatted_release_date, poster_path)
+    movies_in_db.append(new_movie)
+
+model.db.session.add_all(movies_in_db)
+model.db.session.commit()
