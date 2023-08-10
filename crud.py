ed58f3e3ab4dc75a928426fb2c14be1282ad18ce
Bryan Ortega <fbryanortega@gmail.com>
2023-08-10 13:28:27 -0500

    Added display all users functionality

--- crud.py
+++ crud.py
@@ -9,6 +9,11 @@ def create_user(email, password):
 
     return user
 
+def get_users():
+    """Return all users."""
+
+    return User.query.all()
+
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
 
--- server.py
+++ server.py
@@ -35,6 +35,14 @@ def show_movie(movie_id):
 
     return render_template("movie_details.html", movie=movie)
 
+@app.route('/users')
+def all_users():
+    """View all users."""
+
+    users = crud.get_users()
+
+    return render_template("all_users.html", users=users)
+
 if __name__ == "__main__":
     connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
